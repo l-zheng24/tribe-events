c1191e562beedbe54924ce1871ddd90bbf665ebf
Lena Zheng <lenazheng710@outlook.com>
2024-04-20 13:06:43 -0400

    added handling for starting both servers

--- server/index.js
+++ server/index.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+
+app.get("/", (req, res) => {
+    res.send("This is from express.js");
+});
+
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 app.use((req, res) => {
     res.status(200).send('Hello, world!');

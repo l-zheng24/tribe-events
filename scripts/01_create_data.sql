ec8fe03b20e24e2478759e8632cf404e799a9309
Lena Zheng <lenazheng710@outlook.com>
2024-04-20 14:44:17 -0400

    updating database fields

--- scripts/01_create_data.sql
+++ scripts/01_create_data.sql
@@ -6,10 +6,11 @@ CREATE TABLE event_data
     end_time TIME,
     event_date DATE,
     event_loc TEXT NOT NULL,
+    building TEXT NOT NULL,
     short_desc TEXT,
     long_desc TEXT
 );
 
-INSERT INTO event_data (title, start_time, end_time, event_date, event_loc, short_desc, long_desc) VALUES 
-    ('test title', '12:00:00', '14:00:00', '06/01/2024', 'testing location', 'testing short description', 'testing really really really really really really really long description here'),
-    ('test title1', '12:00', '14:00', '07/01/2024', 'testing location', 'testing short description', 'testing really really really really really really really really really really long description here');
+INSERT INTO event_data (title, start_time, end_time, event_date, event_loc, building, short_desc, long_desc) VALUES 
+    ('test title', '12:00:00', '14:00:00', '06/01/2024', 'testing location', 'testing building', 'testing short description', 'testing really really really really really really really long description here'),
+    ('test title1', '12:00', '14:00', '07/01/2024', 'testing location', 'testing building', 'testing short description', 'testing really really really really really really really really really really long description here');

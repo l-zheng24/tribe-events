1e8ad8f3e12bf632a00333a6bd387ea6b2edf021
Lena Zheng <lenazheng710@outlook.com>
2024-04-20 18:47:48 -0400

    connected scraping with server and inserting data into remote pg

--- backend/scraping_scripts/02_.py
+++ backend/scraping_scripts/02_.py
@@ -4,11 +4,12 @@ import json
 with open('data/event_data.json') as f:
     data = json.load(f)
 
-url = "http://localhost:8000/data/"
-
+url = "http://localhost:8080/data/"
+headers = {'Content-Type': 'application/json'}
 try:
-    response = requests.post(url, json=data)
+    response = requests.post(url, json=data, headers=headers)
     response.raise_for_status() 
+    print(response.text)
     print("Post request successful!")
 except requests.exceptions.RequestException as e:
     print("Error occurred during post request:", e)
